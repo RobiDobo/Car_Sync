c137687d08c5e8f9259c7e44fd9325e1e9416a5f
RobiDobo <robidobosan@outlook.com>
2025-10-28 00:37:46 +0000

    Update sync_script.py

--- PythonScripts/sync_script.py
+++ PythonScripts/sync_script.py
@@ -8,10 +8,10 @@ import shutil # Added for clean directory removal
 COOKIES_FILE = "cookies.txt"
 DOWNLOAD_ARCHIVE_FILE = "download_archive.txt" 
 #PLAYLIST_URL = os.getenv("PLAYLIST_URL")
-R2_ACCESS_KEY = os.environ.get('ACCESS_KEY')
-R2_SECRET_KEY = os.environ.get('SECRET_KEY')
-R2_BUCKET = os.environ.get('BUCKET')
-R2_ACCOUNT_ID = os.environ.get('ACCOUNT_ID')
+R2_ACCESS_KEY = os.environ.get('R2_ACCESS_KEY_ID')
+R2_SECRET_KEY = os.environ.get('R2_SECRET_ACCESS_KEY')
+R2_BUCKET = os.environ.get('R2_BUCKET_NAME')
+R2_ACCOUNT_ID = os.environ.get('R2_ACCOUNT_ID')
 R2_ENDPOINT = f"https://{R2_ACCOUNT_ID}.r2.cloudflarestorage.com"
 
 PLAYLISTS_JSON_FILE = "playlists.json"
